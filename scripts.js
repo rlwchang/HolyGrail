b66eeabc3a38f0a42d5e658c11e47831b60592d2
Richard Chang <richardlwchang@gmail.com>
2017-11-21 14:55:38 -0800

    Complete flexbox test project for fCC tribute

---
+++ scripts.js
@@ -0,0 +1,136 @@
+var charData = {
+    red: {
+        archer: {
+            name: "Atalanta",
+            description: "Archer's True Name is Atalanta, a famous huntress known as the Chaste Huntress that appears in Greek Mythology. She is a huntress who is famous for her swift feet in Ancient Greek legend. She became famous after being the first one to drive an arrow into the Calydonian Boar during its extermination. Also, she is included as a member of the Argonauts, which assembled brave heroes from all over Greece. She has the tragic past of being born as the daughter of a king of a certain country and yet being abandoned in the mountains by her father."
+        },
+        assassin: {
+            name: "Semiramis",
+            description: "Assassin's True Name is Semiramis, Wise Queen of Assyria. Born from the Syrian mermaid goddess Derketo and a human man, she was left on the waterfront by her loveless mother. She was wrapped in the warm wings of a dove, and fed with the dove's milk. Semiramis was then found and educated by the shepherd Simmas until she married Onnes, an old general; but she was then taken away by the Syrian king Ninus who fell in love with her thanks to her beautiful face. This forced Onnes to commit suicide, and after Semiramis earned the favors of the king with original battle plans, she married him and formally became queen. A few days after the marriage, she killed the king with poison, and reigned as a regent over Assyria for the next few decades. This is the oldest case of murder by poisoning in legends."
+        },
+        berserker: {
+            name: "Spartacus",
+            description: "Berserker's True Name is Spartacus, the ringleader who raised a large-scale rebellion of slaves in Ancient Rome known as the War of Spartacus. A Thracian gladiator captured by Romans who became a symbol of rebellion. He was affiliated with a gladiator training camp in the Campania region, but escaped with seventy-eight of his comrades in 73 B.C. He managed to repulse an assault force of nearly three thousand dispatched by the Romans, leading to him being known as a hero who inspired armed uprisings by slaves in many places. His army grew to seven thousand men, and they kept defeating the Roman army without losing a single battle. They were unable to cross the Alps and instead decided to head to the island of Sicily. They hired pirates to make the voyage, but were betrayed and attacked by the Roman Legions. He always fought at the head of the front lines, and even when the revolt was subjugated by the army, he fought until his death."
+        },
+        caster: {
+            name: "Shakespeare",
+            description: "Caster's True Name is William Shakespeare (1564-1616), a poet and playwright from the Elizabethan England. His father was an affluent man in Stratford, but it is uncertain if Shakespeare received a higher education. Half of his life is shrouded in mystery and there are many mysteries in his career, such as an undocumented gap of seven years. In his early days as a playwright, his productions revolved around comedy, then historical drama, before he changed his style for magnificent tragedy. At the same time, he also played as an actor in the underground, and he was the target of slander and derision from influential people. In any case, he just had to write a few works and his reputation didn't know where to stop. Considering how he was belittled by a senior playwright as a \"upstart Crow\" at the time, it seems that he was quite envied."
+        },
+        lancer: {
+            name: "Karna",
+            description: "Lancer's True Name is Karna, the Son of the Sun God, the invulnerable hero of the Indian epic Mahabharata, as a hero on the vanquished side. The central conflict of The Mahabharata is the war over influence between the Pandava royal family and Kaurava royal family. Karna became famous as the rival of Arjuna, the great hero of Hindu mythology."
+        },
+        rider: {
+            name: "Achilles",
+            description: "Rider's True Name is Achilles (アキレウス, Akireusu?), one of the famous heroes in the Trojan Wars. Born from the sea goddess Thetis and the hero Peleus, Achilles was blessed by the gods from birth. His mother Thetis, having greatly loved her son, had warmed him within holy flames in order to try and make him immortal. But her husband Peleus had opposed it, saying “Then that would destroy Achilles as a human”, and in the end Achilles grew up while only a certain part of him remained human. Eventually, when war broke out between Troia and Achaea, Achilles was asked the following by his mother Thetis: \"Do you wish to live a long and peaceful life without having your name recognized by the masses? Or do you wish to live the short life of a hero with brilliant achievements in battle?\" Achilles choice went without saying."
+        },
+        saber: {
+            name: "Mordred",
+            description: "Saber\'s True Name is Mordred, The Knight of Treachery, and the \"son\" of King Arthur, but she is actually female despite being raised as the secret male heir to the throne. She became known as a \"rare villain\" who managed to tarnish a glorious legend at the end of her life. She was conceived between Artoria and her sister, Morgan le Fay, through unorthodox circumstances. Artoria, normally female, was a pseudo-male at the time due to Merlin's magecraft, so Morgan enchanted her with a spell to extract sperm from her. Morgan developed it within her own ovary, and made the child into a homunculus clone of Artoria. Due to her status as a homunculus, she was given a much faster growth period than a regular human being, and her life span was far shorter. She was raised in secret and told to hide her status and obey the king until the time was right."
+        }
+    },
+    black: {
+        archer: {
+            name: "Chiron",
+            description: "Archer's True Name is Chiron, a centaur, half man and half horse, who was known as a great sage among his kind in Greek mythology. He is the son of the Titan Cronus, patron of the harvest and the earth, and the deity Philyra, making him a Divine Spirit that would not normally qualify for the status of Heroic Spirit. Philyra ashamed of her offspring's half man and half horse appearance, Philyra was transformed into a linden tree. He received various teachings from the gods such as learning medicine and music from the sun god Apollo and hunting from the goddess Artemis. And so, young and immature heroes gathered around him and learned various teachings and fighting techniques under his wing. He mentored numerous famous heroes starting with Heracles, Jason, Castor and Asclepius. He was an old friend of Peleus, and happily accepted the role of training and mentoring Achilles for nine years."
+        },
+        assassin: {
+            name: "Jack the Ripper",
+            description: "Assassin's True Name is Jack the Ripper, the Legendary Serial Killer. Although there are numerous theories about who Jack the Ripper could have been, she is one interpretation of the series of gory murders in 19th Century England. Her true nature is that of a collection of wraiths born from the aborted children of prostitutes in Whitechapel, London. Because she is a collection of wraiths and not a single entity, piercing her heart will not lead to her immediate death, but the spillage of said wraiths."
+        },
+        berserker: {
+            name: "Frankenstein",
+            description: "Berserker's True Name is Frankenstein, the Frankenstein's monster . Based upon the artificial life created by Victor Frankenstein in Mary Shelley's novel \"Frankenstein: or the Modern Prometheus\". Victor sought to recreate the original couple described in the bible -- Adam and Eve. Due to the limitations of man, he first built Eve, intending for her to give birth to the counterpart. What started as a patchwork of lifeless flesh was given life by the energy harnessed from lightning. Unfortunately, what awoke was not the ideal woman that Victor envisioned, but an abomination with the mind of an infant. Having no developed capacity for expression, her sense of morality was completely absent. After presenting the entrails of a slain dog as a gift for the master, she was branded a failure and disassembled, before being abandoned completely."
+        },
+        caster: {
+            name: "Avicebron",
+            description: "Caster's True Name is Avicebron, also known as Solomon ibn Gabirol, a twelfth-century poet and philosopher. He was born in Malaga, Spain, and although he did not have the glory of a knight or king or the recognition of having produced works of art that would prosper for thousands of years, he was the father of an entire thaumaturgical system, Kabbalah, and one of the starting points of the process leading up to the European Renaissance through bringing Greco-Arabic and Jewish lore and enlightenment to the cultural circles of Europe. He is recognized as a \"hero\" who heavily influenced both the history of the world and thaumaturgy."
+        },
+        lancer: {
+            name: "Vlad",
+            description: "Lancer's True Name is Vlad III, the Lord Impaler (1431-1476), the greatest hero of Wallachia, whose feats in war caused him to be known fearsomely as the \"Impaling Prince\" to the Turks. Due to the legends associated with him, another name has been spread across the world, The Little Dragon, better known as the vampire Count Dracula. While the nation was small, he was a hero who ascended to the throne and managed to turn back the numerous invasions of the Ottoman Empire that had trampled over every other country. Though the model for Dracula, he was not a vampire, but a man of devotion and great hero."
+        },
+        rider: {
+            name: "Astolfo",
+            description: "Rider's True Name is Astolfo , one of the Twelve Paladins of Charlemagne . In the Charlemagne Legend, he is the son of an English king, and one of Charlemagne's twelve faithful Paladins. Among the Twelve Paladins of Charlemagne, Astolfo is said to be the most handsome, eternally optimistic, and completely lacking in sense. As the legends go, Astolfo was quite the ladies' man. As a cousin of Roland, Astolfo is included among those twelve -- that being said, Astolfo was famed as \"weak\" in legends."
+        },
+        saber: {
+            name: "Siegfried",
+            description: "Saber's True Name is Siegfried, the Dragon-Blooded Knight and the \"Dragon Slayer\" who defeated the evil dragon Fafnir with the holy sword Balmung in hand. He is a great national hero of Germany that has many different depictions in the various legends attributed to him. His most famous role is his introductory appearance in the German epic poem of the Middle Ages, the \"Nibelungenlied\", portrayed as its main character. He is a character in a tragedy that had spread throughout Europe concerning dragon slaying and the Rhinegold, and his character became established with the ancient epic \"Nibelungenlied\". However, Siegfried is actually not the main character of the \"Nibelungenlied\". The real main character is his wife Kriemhild, and this epic is the tale of the most terrible revenge brought upon by her despair and hatred."
+        }
+    }
+}
+
+function Selection() {
+    var icons = document.querySelectorAll(".char__icon");
+    var displayImage = document.querySelector(".main__display__image");
+    var displayTitle = document.querySelector(".main__display__title");
+    var displayClass = document.querySelector(".main__display__class")
+    var displayDescription = document.querySelector(".main__display__description");
+
+    var archerRed = icons[0];
+
+    initialize();
+    events();
+
+// Listen to events
+    function events() {
+        icons.forEach(function(icon, index, array) {
+            var faction = icon.getAttribute("data-faction");
+            var servantClass = icon.getAttribute("data-class");
+
+            icon.addEventListener("click", function() {
+                highlightIcon(icon);
+                swapImage(displayImage, getImageSrc(icon));
+                swapText(displayTitle, charData[faction][servantClass]["name"], "main__display__title--invisible");
+                swapText(displayClass, toTitleCase(faction + " " + servantClass), "main__display__class--invisible");
+                swapText(displayDescription, charData[faction][servantClass]["description"], "main__display__description--invisible");
+            });
+        })
+    }
+
+// Methods
+    function initialize(icon) {
+        var icon = archerRed;
+        var faction = icon.getAttribute("data-faction");
+        var servantClass = icon.getAttribute("data-class");
+
+        highlightIcon(icon);
+        swapImage(displayImage, getImageSrc(icon));
+        swapText(displayTitle, charData[faction][servantClass]["name"], "main__display__title--invisible");
+        swapText(displayClass, toTitleCase(faction + " " + servantClass), "main__display__class--invisible");
+        swapText(displayDescription, charData[faction][servantClass]["description"], "main__display__description--invisible");
+    }
+
+    function highlightIcon(selectedIcon) {
+        icons.forEach(function(icon, index, array) {icon.classList.remove("char__icon--active")});
+        selectedIcon.classList.add("char__icon--active");
+    }
+
+    function swapImage(element, src) {
+        element.classList.toggle("main__display__image--invisible");
+        setTimeout(function() {
+            element.setAttribute("src", src);
+            element.classList.toggle("main__display__image--invisible");
+        }, 500);
+    }
+
+    function getImageSrc(element) {
+        var imageSrc = "images/" + element.getAttribute("data-class") + "-" + element.getAttribute("data-faction") + ".png";
+        return imageSrc;
+    }
+
+    function toTitleCase(str) {
+        return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    }
+
+    function swapText(element, text, className) {
+        element.classList.toggle(className);
+        setTimeout(function() {
+            element.textContent = text;
+            element.classList.toggle(className);
+        }, 500);
+    }
+}
+
+new Selection();
